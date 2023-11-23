b2d6fb1c8802618383a24b064e8f938c377c0e3a
Rodrigo Pérez <rodrigo.perez@solucionatica.pe>
2023-11-23 17:07:00 -0500

    HU: Editar Perfil terminada

--- src/app/modules/perfil/components/cambiar-contrasena/cambiar-contrasena.component.ts
+++ src/app/modules/perfil/components/cambiar-contrasena/cambiar-contrasena.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/services/auth/auth.service';
+import { UsuarioService } from 'src/app/core/services/http/usuario/usuario.service';
+import { CustomDialogService } from 'src/app/shared/components/custom-dialog/service/custom-dialog.service';
+import {
+  CustomSnackbarService,
+  TipoSnackbar,
+} from 'src/app/shared/components/custom-snackbar/service/custom-snackbar.service';
+import { FormValidator } from 'src/app/shared/util/validators/formValidator.service';
 
 @Component({
   selector: 'app-cambiar-contrasena',
@@ -7,10 +16,109 @@ import { Component } from '@angular/core';
 })
 export class CambiarContrasenaComponent {
   ocultarActual: boolean = true;
-
   ocultarNueva: boolean = true;
 
-  constructor() {}
+  loading: boolean = false;
 
-  onSubmit() {}
+  contrasenaForm: FormGroup = this.fb.group(
+    {
+      var_contrasena: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(12),
+        ],
+      ],
+      var_contrasena_nueva: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(12),
+        ],
+      ],
+      var_confirmar_nueva: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(12),
+        ],
+      ],
+    },
+    {
+      validators: this.formValidator.validarPasswordConfirmada(
+        'var_contrasena_nueva',
+        'var_confirmar_nueva'
+      ),
+    }
+  );
+
+  constructor(
+    private fb: FormBuilder,
+    private usuarioService: UsuarioService,
+    private formValidator: FormValidator,
+    private _snackbarService: CustomSnackbarService,
+    private authService: AuthService,
+    private dialogService: CustomDialogService
+  ) {}
+
+  validarCampoFormulario(campo: string) {
+    return this.formValidator.validarCampoFormulario(
+      this.contrasenaForm,
+      campo
+    );
+  }
+
+  obtenerErrorCampoFormulario(campo: string) {
+    return this.formValidator.errorCampoFormulario(this.contrasenaForm, campo);
+  }
+
+  abrirSnackbar(icono: string, mensaje: string, tipo: TipoSnackbar) {
+    this._snackbarService.loadSnackBar(icono, mensaje, tipo);
+  }
+
+  onSubmit() {
+    if (this.contrasenaForm.invalid) {
+      return this.contrasenaForm.markAllAsTouched();
+    }
+
+    const dialogRef = this.dialogService.loadDialog(
+      '¿Estás seguro de que quieres cambiar tu contraseña?',
+      'Al actualizar su contraseña deberá volver a iniciar sesión. Si estás seguro, haz clic en "Confirmar" de lo contrario, puedes cancelar esta acción.',
+      true
+    );
+
+    dialogRef.subscribe((res) => {
+      if (res) {
+        this.usuarioService
+          .actualizarContrasena(this.contrasenaForm.value)
+          .subscribe({
+            next: (_) => {
+              this.loading = true;
+
+              this.abrirSnackbar(
+                'check_circle',
+                'Contraseña actualizada exitosamente',
+                'success'
+              );
+
+              setTimeout(() => {
+                this.authService.cerrarSesion();
+              }, 1500);
+            },
+            error: (err) => {
+              const { error } = err;
+
+              if (error.invalidPassword) {
+                this.contrasenaForm
+                  .get('var_contrasena')
+                  ?.setErrors({ invalidPassword: true });
+              }
+            },
+          });
+      }
+    });
+  }
 }
--- src/app/modules/perfil/components/editar-perfil/editar-perfil.component.ts
+++ src/app/modules/perfil/components/editar-perfil/editar-perfil.component.ts
@@ -1,14 +1,114 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UsuarioService } from 'src/app/core/services/http/usuario/usuario.service';
+import { CustomDialogService } from 'src/app/shared/components/custom-dialog/service/custom-dialog.service';
+import {
+  CustomSnackbarService,
+  TipoSnackbar,
+} from 'src/app/shared/components/custom-snackbar/service/custom-snackbar.service';
+import { FormValidator } from 'src/app/shared/util/validators/formValidator.service';
 
 @Component({
   selector: 'app-editar-perfil',
   templateUrl: './editar-perfil.component.html',
   styleUrls: ['./editar-perfil.component.css'],
 })
-export class EditarPerfilComponent {
+export class EditarPerfilComponent implements OnInit {
   ocultarPassword: boolean = true;
 
-  constructor() {}
+  loading: boolean = false;
 
-  onSubmit() {}
+  perfilForm: FormGroup = this.fb.group({
+    var_nombres: ['', [Validators.required, Validators.minLength(3)]],
+    var_apellidos: ['', [Validators.required, Validators.minLength(3)]],
+    var_correo: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(this.formValidator.correoPatron),
+      ],
+    ],
+    var_contrasena: ['', [Validators.required]],
+  });
+
+  constructor(
+    private usuarioService: UsuarioService,
+    private fb: FormBuilder,
+    private formValidator: FormValidator,
+    private dialogService: CustomDialogService,
+    private _snackbarService: CustomSnackbarService
+  ) {}
+
+  ngOnInit(): void {
+    this.obtenerDatosPerfil();
+  }
+
+  obtenerDatosPerfil() {
+    this.usuarioService.obtenerDatosPerfil().subscribe({
+      next: (res) => {
+        this.perfilForm.patchValue(res);
+      },
+    });
+  }
+
+  validarCampoFormulario(campo: string) {
+    return this.formValidator.validarCampoFormulario(this.perfilForm, campo);
+  }
+
+  obtenerErrorCampoFormulario(campo: string) {
+    return this.formValidator.errorCampoFormulario(this.perfilForm, campo);
+  }
+
+  abrirSnackbar(icono: string, mensaje: string, tipo: TipoSnackbar) {
+    this._snackbarService.loadSnackBar(icono, mensaje, tipo);
+  }
+
+  onSubmit() {
+    if (this.perfilForm.invalid) {
+      return this.perfilForm.markAllAsTouched();
+    }
+
+    if (
+      this.perfilForm.get('var_correo')?.pristine &&
+      this.perfilForm.get('var_nombres')?.pristine &&
+      this.perfilForm.get('var_apellidos')?.pristine
+    ) {
+      return this.abrirSnackbar(
+        'warning',
+        'No se detectaron cambios',
+        'warning'
+      );
+    }
+
+    const dialogRef = this.dialogService.loadDialog(
+      '¿Desea actualizar sus datos?',
+      'Al actualizar sus datos, estos se verán reflejados en su perfil automaticamente. Si estás seguro, haz clic en "Confirmar" de lo contrario, puedes cancelar esta acción.',
+      true
+    );
+
+    dialogRef.subscribe((resp) => {
+      if (resp) {
+        this.usuarioService.actualizarPerfil(this.perfilForm.value).subscribe({
+          next: (_) => {
+            this.loading = true;
+
+            this.formValidator.handleResponse(
+              this.perfilForm,
+              'Datos actualizados correctamente',
+              '/perfil'
+            );
+          },
+          error: (err) => {
+            const { error } = err;
+
+            if (error.invalidPassword) {
+              this.perfilForm
+                .get('var_contrasena')
+                ?.setErrors({ invalidPassword: true });
+            }
+          },
+        });
+      }
+    });
+  }
 }
